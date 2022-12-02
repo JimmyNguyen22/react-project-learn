f77f780b33118f7936d80c5998bc9a90b6aa7175
jimmy <vphu456@gmail.com>
2022-12-02 12:54:46 +0700

    11 project react

---
+++ 12-sidebar-modal/setup/src/Sidebar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import logo from "./logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
+import { useGlobalContext } from "./context";
+
+const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+
+  return (
+    <aside className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}>
+      <div className="sidebar-header">
+        <img src={logo} className="logo" alt="logo" />
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          return (
+            <li key={link.id}>
+              <a href={link.url}>
+                {link.icon} {link.text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+
+      <div className="social-icons">
+        {social.map((link) => {
+          return (
+            <li key={link.id}>
+              <a href={link.url}>{link.icon}</a>
+            </li>
+          );
+        })}
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
---
+++ 15-cocktails/setup/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt libero nam repellat hic iusto commodi praesentium id dicta quas beatae saepe delectus incidunt, eius ratione voluptatum explicabo, accusantium ullam. Iste!</p>
+    </section>
+  );
+};
+
+export default About;
